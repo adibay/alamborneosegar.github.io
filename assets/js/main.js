ed3489ff4b8f4bfc685a5fabc50e7e50d3d07d39
adibay <adib01295@gmail.com>
2021-11-27 08:14:05 +0700

    edit produk

--- assets/js/main.js
+++ assets/js/main.js
@@ -226,6 +226,48 @@
       }
     }
   });
+
+    /**
+   * Our Products slider
+   */
+     new Swiper('.ourproducts-slider', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+  
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        }
+      }
+    });
+  
+    /**
+     * Animation on scroll
+     */
+    window.addEventListener('load', () => {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      })
+    });
+    
   /**
    * products isotope and filter
    */
